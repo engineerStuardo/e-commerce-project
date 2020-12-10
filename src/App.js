33b793d2b7c62c8c9c0f44fdb2150f80e10e15ab
icortez <icortez@ufg.edu.sv>
2020-12-10 13:31:29 -0600

    adding an input-component for signin-component and adding some style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.scss';
-import HomePage from './pages/homepage/HomePage';
 import { Route, Switch } from 'react-router-dom';
+
+import './App.scss';
+
+import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/header';
+import SignInAndOutPage from './pages/sign-in-and-up/sign-in-and-up';
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndOutPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-up/sign-in-and-up.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sign-in-and-up.scss';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+const SignInAndUpPage = () => (
+  <div className='sign-in-and-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndUpPage;
