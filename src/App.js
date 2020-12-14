7fb9fe806728c8d9be152e09c75b3ef16bf80de2
icortez <icortez@ufg.edu.sv>
2020-12-14 13:25:09 -0600

    implemented firebase utils, included ability to store authenticated user into our firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/header';
 import SignInAndOutPage from './pages/sign-in-and-up/sign-in-and-up';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   state = {
@@ -17,8 +17,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShop => {
+          this.setState({
+            currentUser: {
+              id: snapShop.id,
+              ...snapShop.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/pages/sign-in-and-up/sign-in-and-up.jsx
+++ src/pages/sign-in-and-up/sign-in-and-up.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './sign-in-and-up.scss';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 const SignInAndUpPage = () => (
   <div className='sign-in-and-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
