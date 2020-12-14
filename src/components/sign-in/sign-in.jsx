e9b097a5f69e0fe9d4304ed123153aa464bc18c1
icortez <icortez@ufg.edu.sv>
2020-12-14 15:55:51 -0600

    working on hiding sign in and up component after log in correctly

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { withRouter } from 'react-router-dom';
 
 import './sign-in.scss';
 
@@ -7,10 +8,14 @@ import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
 
 class SignIn extends React.Component {
-  state = {
-    email: '',
-    password: '',
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
   handleSubmit = async event => {
     event.preventDefault();
@@ -24,9 +29,12 @@ class SignIn extends React.Component {
         password: '',
       });
     } catch (e) {
+      alert(e);
       console.log(e);
+      return;
     }
     this.setState({ email: '', password: '' });
+    this.props.history.push('/');
   };
 
   handleChange = event => {
@@ -60,7 +68,10 @@ class SignIn extends React.Component {
 
           <div className='buttons'>
             <CustomButton type='submit'>Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              onClick={() => signInWithGoogle(this.props)}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
@@ -70,4 +81,4 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+export default withRouter(SignIn);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -44,6 +44,9 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = async props => {
+  await auth.signInWithPopup(provider);
+  props.history.push('/');
+};
 
 export default firebase;
