5fb3df9dfcd98d879907388e8a87378d86f9906c
icortez <icortez@ufg.edu.sv>
2021-01-04 11:32:05 -0600

    adding cart icon without functionality

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,6 +6,7 @@ import './header.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
