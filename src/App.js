a39c1124a33549904452dc06e5289109ee600483
icortez <icortez@ufg.edu.sv>
2021-01-11 14:43:11 -0600

    added checkout page, checkout item component and updated cart to use selector from reselect

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import './App.scss';
 
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
-import Header from './components/header/header';
 import SignInAndOutPage from './pages/sign-in-and-up/sign-in-and-up';
+import CheckoutPage from './pages/checkout/checkout';
+
+import Header from './components/header/header';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user-actions';
 import { selectCurrentUser } from './redux/user/user-selector';
 
@@ -47,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
