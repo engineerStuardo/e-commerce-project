306af7a57655ce556c0ec43a1e714e02a51b750a
icortez <icortez@ufg.edu.sv>
2021-01-28 15:20:31 -0600

    added animation to HomePageContainer in home-page-component.jsx

--- src/pages/homepage/home-page-styles.jsx
+++ src/pages/homepage/home-page-styles.jsx
@@ -4,4 +4,16 @@ export const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  animation: show 1.8s ease-in-out 1;
+
+  @keyframes show {
+    0% {
+      transform: scale(1.05);
+      opacity: 0;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
 `;
