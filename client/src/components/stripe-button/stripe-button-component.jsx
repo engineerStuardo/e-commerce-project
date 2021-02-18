2ba391932b03a8d88d24602e406dbb17a2b54a3e
icortez <icortez@ufg.edu.sv>
2021-02-18 10:46:38 -0600

    We have separated out our front-end client from our back-end express server that handles our stripe charges

--- client/src/components/stripe-button/stripe-button-component.jsx
+++ client/src/components/stripe-button/stripe-button-component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import { useAlert } from 'react-alert';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -9,8 +10,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51I9xtxIAD7VzYoW4xb83C7cbf095x1myhqqoDl3h2EuZr5TzyJmZDfl2K2b54goK8TUh7CdLjE5aODhIlRMl60v600YRkgUVFm';
 
   const onToken = token => {
-    console.log(token);
-    alert.success('Successful Payment!!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert.success('Successful Payment!!');
+      })
+      .catch(error => {
+        console.log(error);
+        alert.error(
+          'There was an issue with yout payment. Please make sure you use the priveded credit card'
+        );
+      });
   };
 
   return (
